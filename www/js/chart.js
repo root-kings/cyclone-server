d25736a81c2953381298fb19683735bbbc128afd
krushndayshmookh <krushndayshmookh@gmail.com>
2018-10-27 08:39:22 +0530

    beautify and add updater

--- www/js/chart.js
+++ www/js/chart.js
@@ -1,7 +1,7 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var gradient = ctx.createLinearGradient(0, 0, 0, 700)
-    gradient.addColorStop(0, '#ffcc33')
-    gradient.addColorStop(1, '#ffb347');
+gradient.addColorStop(0, '#ffcc33')
+gradient.addColorStop(1, '#ffb347');
 
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -9,11 +9,11 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ["Rider 1","Rider 2","Rider 3","Rider 4","Rider 5"],
+        labels: ["Rider 1", "Rider 2", "Rider 3", "Rider 4", "Rider 5"],
         datasets: [{
             label: "Rider {num}",
             backgroundColor: gradient,
-            data: [45,45,45,45,45,45, 45],
+            data: [45, 45, 45, 45, 45],
         }]
     },
 
@@ -27,9 +27,14 @@ var chart = new Chart(ctx, {
             }],
             xAxes: [{
                 // Change here
-            	barPercentage: 0.3
+                barPercentage: 0.3
             }]
         }
     },
 
 });
+
+function setValues (data) {
+    chart.data.datasets.data = data;
+    chart.update();
+}
