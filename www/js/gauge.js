c1083a6b62cd89a374d450b4ad8a1e623c19ede6
Sahil Kindarle <shieldanima@MacBook-Air.local>
2018-10-28 07:49:43 +0530

    uske hisab ka UI

--- www/js/gauge.js
+++ www/js/gauge.js
@@ -16,9 +16,122 @@ var opts = {
   highDpiSupport: true,     // High resolution support
 
 };
+
+var opts1 = {
+  angle: 0.15, // The span of the gauge arc
+  lineWidth: 0.44, // The line thickness
+  radiusScale: 1, // Relative radius
+  pointer: {
+    length: 0.6, // // Relative to gauge radius
+    strokeWidth: 0.035, // The thickness
+    color: '#000000' // Fill color
+  },
+  limitMax: false,     // If false, max value increases automatically if value > maxValue
+  limitMin: false,     // If true, the min value of the gauge will be fixed
+  colorStart: '#6FADCF',   // Colors
+  colorStop: '#8FC0DA',    // just experiment with them
+  strokeColor: '#E0E0E0',  // to see which ones work best for you
+  generateGradient: true,
+  highDpiSupport: true,     // High resolution support
+
+};
+
+
+var opts2 = {
+  angle: 0.15, // The span of the gauge arc
+  lineWidth: 0.44, // The line thickness
+  radiusScale: 1, // Relative radius
+  pointer: {
+    length: 0.6, // // Relative to gauge radius
+    strokeWidth: 0.035, // The thickness
+    color: '#000000' // Fill color
+  },
+  limitMax: false,     // If false, max value increases automatically if value > maxValue
+  limitMin: false,     // If true, the min value of the gauge will be fixed
+  colorStart: '#6FADCF',   // Colors
+  colorStop: '#8FC0DA',    // just experiment with them
+  strokeColor: '#E0E0E0',  // to see which ones work best for you
+  generateGradient: true,
+  highDpiSupport: true,     // High resolution support
+
+};
+
+
+var opts3 = {
+  angle: 0.15, // The span of the gauge arc
+  lineWidth: 0.44, // The line thickness
+  radiusScale: 1, // Relative radius
+  pointer: {
+    length: 0.6, // // Relative to gauge radius
+    strokeWidth: 0.035, // The thickness
+    color: '#000000' // Fill color
+  },
+  limitMax: false,     // If false, max value increases automatically if value > maxValue
+  limitMin: false,     // If true, the min value of the gauge will be fixed
+  colorStart: '#6FADCF',   // Colors
+  colorStop: '#8FC0DA',    // just experiment with them
+  strokeColor: '#E0E0E0',  // to see which ones work best for you
+  generateGradient: true,
+  highDpiSupport: true,     // High resolution support
+
+};
+
+
+var opts4 = {
+  angle: 0.15, // The span of the gauge arc
+  lineWidth: 0.44, // The line thickness
+  radiusScale: 1, // Relative radius
+  pointer: {
+    length: 0.6, // // Relative to gauge radius
+    strokeWidth: 0.035, // The thickness
+    color: '#000000' // Fill color
+  },
+  limitMax: false,     // If false, max value increases automatically if value > maxValue
+  limitMin: false,     // If true, the min value of the gauge will be fixed
+  colorStart: '#6FADCF',   // Colors
+  colorStop: '#8FC0DA',    // just experiment with them
+  strokeColor: '#E0E0E0',  // to see which ones work best for you
+  generateGradient: true,
+  highDpiSupport: true,     // High resolution support
+
+};
+
+
 var target = document.getElementById('foo'); // your canvas element
 var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
 gauge.maxValue = 3000; // set max gauge value
 gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
 gauge.animationSpeed = 32; // set animation speed (32 is default value)
 gauge.set(1250); // set actual value
+
+
+var target1 = document.getElementById('foo1'); // your canvas element
+var gauge1 = new Gauge(target1).setOptions(opts1); // create sexy gauge!
+gauge1.maxValue = 3000; // set max gauge value
+gauge1.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gauge1.animationSpeed = 32; // set animation speed (32 is default value)
+gauge1.set(1250); // set actual value
+
+
+var target2 = document.getElementById('foo2'); // your canvas element
+var gauge2 = new Gauge(target2).setOptions(opts2); // create sexy gauge!
+gauge2.maxValue = 3000; // set max gauge value
+gauge2.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gauge2.animationSpeed = 32; // set animation speed (32 is default value)
+gauge2.set(1250); // set actual value
+
+
+var target3 = document.getElementById('foo3'); // your canvas element
+var gauge3 = new Gauge(target3).setOptions(opts3); // create sexy gauge!
+gauge3.maxValue = 3000; // set max gauge value
+gauge3.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gauge3.animationSpeed = 32; // set animation speed (32 is default value)
+gauge3.set(1250); // set actual value
+
+
+var target4 = document.getElementById('foo4'); // your canvas element
+var gauge4 = new Gauge(target4).setOptions(opts4); // create sexy gauge!
+gauge4.maxValue = 3000; // set max gauge value
+gauge4.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gauge4.animationSpeed = 32; // set animation speed (32 is default value)
+gauge4.set(1250); // set actual value
