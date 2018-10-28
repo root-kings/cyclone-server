3334d1ce2307f01aae0c93ff7b4f16bd1ec3315c
krushndayshmookh <krushndayshmookh@gmail.com>
2018-10-28 20:43:14 +0530

    fix progress bar reverse

--- www/js/index.js
+++ www/js/index.js
@@ -1,17 +1,16 @@
-
 function openNav() {
-  document.getElementById("Splash").style.width = "100%";
+    document.getElementById("Splash").style.width = "100%";
 }
 
 function closeNav() {
-  document.getElementById("Splash").style.width = "0%";
+    document.getElementById("Splash").style.width = "0%";
 }
 
 
 function setBar(distances) {
-  document.getElementById("bar0").style.height = JSON.stringify(distances[0])+"px";
-  document.getElementById("bar1").style.height = JSON.stringify(distances[1])+"px";
-  document.getElementById("bar2").style.height = JSON.stringify(distances[2])+"px";
-  document.getElementById("bar3").style.height = JSON.stringify(distances[3])+"px";
-  document.getElementById("bar4").style.height = JSON.stringify(distances[4])+"px";
+    document.getElementById("bar0").style.height = JSON.stringify(400 - distances[0]) + "px";
+    document.getElementById("bar1").style.height = JSON.stringify(400 - distances[1]) + "px";
+    document.getElementById("bar2").style.height = JSON.stringify(400 - distances[2]) + "px";
+    document.getElementById("bar3").style.height = JSON.stringify(400 - distances[3]) + "px";
+    document.getElementById("bar4").style.height = JSON.stringify(400 - distances[4]) + "px";
 }
