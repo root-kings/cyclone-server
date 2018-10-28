2aaa692ae854bbaa1d0dc38ea4090502916441ba
krushndayshmookh <krushndayshmookh@gmail.com>
2018-10-28 14:16:18 +0530

    bar and guage

--- www/js/gauge.js
+++ www/js/gauge.js
@@ -97,41 +97,50 @@ var opts4 = {
 };
 
 
-var target = document.getElementById('foo'); // your canvas element
-var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-gauge.maxValue = 3000; // set max gauge value
-gauge.setMinValue(0); // Prefer setter over gauge.minValue = 0
-gauge.animationSpeed = 32; // set animation speed (32 is default value)
-gauge.set(1250); // set actual value
+var target = document.getElementById('foo0'); // your canvas element
+var gauge0 = new Gauge(target).setOptions(opts); // create sexy gauge!
+gauge0.maxValue = 20; // set max gauge value
+gauge0.setMinValue(0); // Prefer setter over gauge.minValue = 0
+gauge0.animationSpeed = 32; // set animation speed (32 is default value)
+gauge0.set(5); // set actual value
 
 
 var target1 = document.getElementById('foo1'); // your canvas element
 var gauge1 = new Gauge(target1).setOptions(opts1); // create sexy gauge!
-gauge1.maxValue = 3000; // set max gauge value
+gauge1.maxValue = 20; // set max gauge value
 gauge1.setMinValue(0);  // Prefer setter over gauge.minValue = 0
 gauge1.animationSpeed = 32; // set animation speed (32 is default value)
-gauge1.set(1250); // set actual value
+gauge1.set(5); // set actual value
 
 
 var target2 = document.getElementById('foo2'); // your canvas element
 var gauge2 = new Gauge(target2).setOptions(opts2); // create sexy gauge!
-gauge2.maxValue = 3000; // set max gauge value
+gauge2.maxValue = 20; // set max gauge value
 gauge2.setMinValue(0);  // Prefer setter over gauge.minValue = 0
 gauge2.animationSpeed = 32; // set animation speed (32 is default value)
-gauge2.set(1250); // set actual value
+gauge2.set(5); // set actual value
 
 
 var target3 = document.getElementById('foo3'); // your canvas element
 var gauge3 = new Gauge(target3).setOptions(opts3); // create sexy gauge!
-gauge3.maxValue = 3000; // set max gauge value
+gauge3.maxValue = 20; // set max gauge value
 gauge3.setMinValue(0);  // Prefer setter over gauge.minValue = 0
 gauge3.animationSpeed = 32; // set animation speed (32 is default value)
-gauge3.set(1250); // set actual value
+gauge3.set(5); // set actual value
 
 
 var target4 = document.getElementById('foo4'); // your canvas element
 var gauge4 = new Gauge(target4).setOptions(opts4); // create sexy gauge!
-gauge4.maxValue = 3000; // set max gauge value
+gauge4.maxValue = 20; // set max gauge value
 gauge4.setMinValue(0);  // Prefer setter over gauge.minValue = 0
 gauge4.animationSpeed = 32; // set animation speed (32 is default value)
-gauge4.set(1250); // set actual value
+gauge4.set(5); // set actual value
+
+
+function setGuage(speeds) {
+    gauge0.set(speeds[0]);
+    gauge1.set(speeds[1]);
+    gauge2.set(speeds[2]);
+    gauge3.set(speeds[3]);
+    gauge4.set(speeds[4]);
+}
