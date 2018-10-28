08119b52a94a0d785a77e11b70c86c53de8b6d3e
krushndayshmookh <krushndayshmookh@gmail.com>
2018-10-28 16:58:07 +0530

    setbar func

--- www/js/index.js
+++ www/js/index.js
@@ -6,3 +6,12 @@ function openNav() {
 function closeNav() {
   document.getElementById("Splash").style.width = "0%";
 }
+
+
+function setBar(distances) {
+  document.getElementById("bar0").style.height = JSON.stringify(distances[0])+"px";
+  document.getElementById("bar1").style.height = JSON.stringify(distances[1])+"px";
+  document.getElementById("bar2").style.height = JSON.stringify(distances[2])+"px";
+  document.getElementById("bar3").style.height = JSON.stringify(distances[3])+"px";
+  document.getElementById("bar4").style.height = JSON.stringify(distances[4])+"px";
+}
