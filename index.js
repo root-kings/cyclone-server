bb4836e74839a7b1e602345abd3bbc0dc3ce4bd2
krushndayshmookh <krushndayshmookh@gmail.com>
2018-11-01 17:11:52 +0530

    lots of fuckups

--- index.js
+++ index.js
@@ -71,26 +71,6 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/www/index.html');
 });
 
-app.get('/reset', function(req, res) {
-    sendableObject = [{
-        meters: 0,
-        kmph: 0
-    }, {
-        meters: 0,
-        kmph: 0
-    }, {
-        meters: 0,
-        kmph: 0
-    }, {
-        meters: 0,
-        kmph: 0
-    }, {
-        meters: 0,
-        kmph: 0
-    }];
-    res.send(true);
-    console.log("Reset done.");
-});
 
 var oldRounds = [0, 0, 0, 0, 0, 0];
 var sendableObject = [{
@@ -110,12 +90,68 @@ var sendableObject = [{
     kmph: 0
 }];
 
+
+
+
+
+
+
+app.get('/reset', function (req, res) {
+    sendableObject = [{
+        meters: 0,
+        kmph: 0
+    }, {
+        meters: 0,
+        kmph: 0
+    }, {
+        meters: 0,
+        kmph: 0
+    }, {
+        meters: 0,
+        kmph: 0
+    }, {
+        meters: 0,
+        kmph: 0
+    }];
+
+
+    sendable = [{
+        meters: 0,
+        kmph: 0
+    }, {
+        meters: 0,
+        kmph: 0
+    }, {
+        meters: 0,
+        kmph: 0
+    }, {
+        meters: 0,
+        kmph: 0
+    }, {
+        meters: 0,
+        kmph: 0
+    }];
+
+    oldRounds = [0, 0, 0, 0, 0, 0];
+
+    res.send(true);
+
+
+
+    console.log("Reset done.");
+
+    console.log(sendableObject, oldRounds);
+});
+
+
+
+
 io.on('connection', function (socket) {
     socket.emit('news', {
         hello: 'world'
     });
 
- 
+
 });
 
 
@@ -123,10 +159,10 @@ function presentor(rounds, x) {
     var diff = rounds - oldRounds[x];
     oldRounds[x] = rounds;
 
-    var m = sendableObject[x].meters + (diff * 2 * 3.14 * 0.30); //this 0.30 is diameter
+    //var m = sendableObject[x].meters + (diff * 2 * 3.14 * 0.30); //this 0.30 is diameter
 
     sendableObject[x] = {
-        meters: m,
+        meters: rounds * 2 * 3.14 * 0.30,
         kmph: diff * 60 * 60 / 1000
     }
 }
@@ -153,12 +189,6 @@ parser1.on('data', function (data) {
     presentor(data[0], 2);
     presentor(data[1], 3);
 
-    // console.log(data);
-
-    // io.emit('data', sendableObject);
-
-    // console.log(sendableObject + " sent.");
-
 });
 
 
@@ -169,19 +199,44 @@ parser2.on('data', function (data) {
     presentor(data[0], 4);
     presentor(data[1], 5);
 
-    // console.log(data);
-
-    // io.emit('data', sendableObject);
-
-    // console.log(sendableObject + " sent.");
-
 });
 
 
 // Debug =====
 
+
+var sendable = [{
+    meters: 0,
+    kmph: 0
+}, {
+    meters: 0,
+    kmph: 0
+}, {
+    meters: 0,
+    kmph: 0
+}, {
+    meters: 0,
+    kmph: 0
+}, {
+    meters: 0,
+    kmph: 0
+}];
+
 function emmiter() {
+    // io.emit('data', sendableObject);
     io.emit('data', sendableObject);
+
+    sendable[1].meters += (Math.random() * 50);
+    sendable[1].kmph = ((Math.random() * 4) + 15);
+    sendable[0].meters += (Math.random() * 50);
+    sendable[0].kmph = ((Math.random() * 4) + 15);
+    sendable[2].meters += (Math.random() * 50);
+    sendable[2].kmph = ((Math.random() * 4) + 15);
+    sendable[3].meters += (Math.random() * 50);
+    sendable[3].kmph = ((Math.random() * 4) + 15);
+    sendable[4].meters += (Math.random() * 50);
+    sendable[4].kmph = ((Math.random() * 4) + 15);
+
 
     console.log(JSON.stringify(sendableObject) + " sent. [-- " + JSON.stringify(new Date) + " --]");
 }
